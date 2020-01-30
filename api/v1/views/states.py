e41f56f73b090a9e5244f3a8bb053e125c581130
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-01-29 19:54:17 -0500

    (docs) api endpoint for the state class

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -10,6 +10,9 @@ from api.v1.views import app_views
 
 @app_views.route('/states')
 def states():
+    """
+    Lists all the State instances.
+    """
     all_states = [State.to_dict(obj) for obj in
                   storage.all("State").values()]
     return jsonify(all_states)
@@ -17,6 +20,13 @@ def states():
 
 @app_views.route('/states/<id>', methods=['GET'])
 def get(id):
+    """
+    Gets a State instance by its ID. If no State is found, the function will
+    return a 404 response.
+
+    Arguments:
+        id (str): State
+    """
     state = storage.get("State", id).to_dict()
     if not state:
         return "Not found", 404
@@ -24,7 +34,16 @@ def get(id):
 
 
 @app_views.route('/states', methods=['POST'])
-def create(id):
+def create():
+    """
+    Creates a State instance.
+
+    If the JSON doesn't contain a "name" entry, the function will return
+    a 400 response.
+
+    If the request contains an invalid JSON, the function will return a
+    400 response.
+    """
     attributes = request.get_json()
     if not attributes:
         return "Not a JSON", 400
@@ -39,6 +58,14 @@ def create(id):
 
 @app_views.route('/states/<id>', methods=['PUT'])
 def update(id):
+    """
+    Updates a State instance.
+
+    If the request contains an invalid JSON, the function will return a
+    400 response.
+
+    If no state is found, the function will return a 404 response.
+    """
     attributes = request.get_json()
     if not attributes:
         return "Not a JSON", 400
@@ -60,6 +87,11 @@ def update(id):
 
 @app_views.route('/states/<id>', methods=['DELETE'])
 def delete(id):
+    """
+    Deletes a State instance.
+
+    If no state is found, the function will return a 404 response.
+    """
     state = storage.get("State", id)
     if not state:
         return "Not found", 404
