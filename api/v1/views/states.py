4d5247387658d9baa46dbc2472365d6b7c10aba7
nickmoran06 <956@holbertonschool.com>
2020-01-31 04:22:13 +0000

    (style) Improving comments, spaces and organization

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 """
-Defines the API States endpoint.
+Defines the API States endpoint
 """
+
 from flask import jsonify, request, abort
 from models import storage
 from models.state import State
