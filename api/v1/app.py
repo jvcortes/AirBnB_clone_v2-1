baada2d6ba744eb5743a286d3424031e8265c1a8
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-01-30 22:40:35 -0500

    (fix) cors allowed origins

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ from api.v1.views import app_views
 app = Flask('__main__')
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
